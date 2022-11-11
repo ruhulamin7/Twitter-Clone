01cfd6b515c2da3a8cf3952e5a8b7a4b4b3784dd
ruhulaminbd93 <ruhulaminbd93@gmail.com>
2022-11-11 14:18:25 +0600

    otp

--- controllers/auth/verifyOTPHandler.js
+++ controllers/auth/verifyOTPHandler.js
@@ -1,13 +1,16 @@
 const createHttpError = require('http-errors');
 const OTP = require('../../models/OTP');
-const User = require('../../models/User');
 
 const verifyOTPHandler = async (req, res, next) => {
   try {
     if (req.isValidOtp) {
       res.render('pages/auth/createNewPassword', {
         error: {},
-        user: { otp: req.otp, otpId: req.otpId, email: req.email },
+        user: {
+          otp: req.otp,
+          otpId: req.otpId,
+          email: req.email,
+        },
       });
     } else {
       next(createHttpError(500, 'Internal Server Error!'));
